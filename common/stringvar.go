240dabe7c4c4c40fa4a397b9fab8d4a9a34cb3f7
Inshal Khan <inshal@bugsmirror.com>
2023-11-04 01:16:54 +0530

    Initial Commit

--- common/stringvar.go
+++
@@ -1,7 +0,0 @@
-package common
-
-const (
-	BackSlash     = "/"
-	GitCreatePath = "https://api.github.com/repos/"
-	Contents      = "contents"
-)
--- go.mod
+++
@@ -1,5 +0,0 @@
-module daem
-
-go 1.20
-
-require github.com/google/go-github/v50 v50.0.0 // indirect
